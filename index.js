529fd5b6fec25570dcc71ec590deb22e42aa5403
Chris Jacobi <106036496+ChrisJacobi@users.noreply.github.com>
2022-10-13 17:56:15 -0400

    cors added, more http methods added

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 
 const express = require('express');
 const app = express();
-app.use(express.json());
-const PORT = 80;
 
+const PORT = 80;
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors({
+    orgin: '*'
+}));
 
 
 let id = 1;
 
-// Rank Entry
+// Rank Entry Example
 const rankEntry = {
-    id: id,
-    topic: "My favorite cats",
-    1: "Calico",
-    2: "Tabby",
-    3: "Sphynx",
-    4: "Siamese",
-    5: "Maine Coon",
-    createdAt: new Date()
-}
+    content: {
+        id: id,
+        topic: "My favorite cats",
+        1: "Calico",
+        2: "Tabby",
+        3: "Sphynx",
+        4: "Siamese",
+        5: "Maine Coon",
+        createdAt: new Date()
+    }
+};
 
-const entries = [];
+let entries = [];
 
-app.get('/', (req, res) => {
+app.get('/entries', (req, res) => {
     res.send(entries);
+});
+
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const entry = entries.find(e => e.id === +id);
+    res.send(entry);
 });
 
 app.post('/entry', (req, res) => {
@@ -32,7 +45,15 @@ app.post('/entry', (req, res) => {
     entry.createdAt = new Date()
     entries.push(entry);
     res.send(entry);
-})
+});
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const index = entries.findIndex(e => e.id === +id);
+    entries.splice(index, 1);
+    res.send(`Deleted Entry with id: '${id}'.`)
+});
+
 
 app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`);

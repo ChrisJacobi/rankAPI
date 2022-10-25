0b51f49aae3a96408b5b62a9f07930ee47c12c9a
Chris Jacobi <106036496+ChrisJacobi@users.noreply.github.com>
2022-10-24 20:34:22 -0400

    connected to mongoDb, still needs bug fixes

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
 
+// EXPRESS SERVER
 const express = require('express');
 const app = express();
-
-const PORT = 80;
-const cors = require('cors');
-
-
 app.use(express.json());
+const PORT = 80;
+
+
+// CORS
+const cors = require('cors');
 app.use(cors({origin: '*'}));
 
 
 
-let id = 1;
-entries = [{
-    content: ''
-}];
+// MONGODB
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://RankProject:yVTkclkkmsyf4moh@cluster0.vbatj02.mongodb.net/?retryWrites=true&w=majority";
 
-app.get('/entries', (req, res) => {
-    res.send(entries);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
-app.get('/:id', (req, res) => {
-    const id = req.params.id;
-    const entry = entries.find(e => e.id === +id);
-    res.send(entry);
+const database = client.db('RankBank');
+const collection = database.collection('ranks');
+
+
+
+app.get('/entries', async (req, res) => {
+    // console.log(req.body)
+    const entries = await collection.find().toArray()
+    res.send(entries)
 });
 
-app.post('/entry', (req, res) => {
-    const entry = req.body;
-    entry.id = id++;
-    entry.createdAt = new Date();
-    entries.push(entry);
-    res.send(entry);
+// app.get('/:id', (req, res) => {
+//    
+// });
+
+app.post('/entry', async (req, res) => {
+    // console.log(req.body)
+    await collection.insertOne(req.body)
+    // console.log(req.body)
+    res.send(req.body)
 });
 
-app.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    const index = entries.findIndex(e => e.id === +id);
-    entries.splice(index, 1);
-    res.send(`Deleted Entry with id: '${id}'.`)
-});
+// app.delete('/:id', (req, res) => {
+//   
+// });
 
-
-app.listen(PORT, () => {
-    console.log(`App is listening at http://localhost:${PORT}`);
+client.connect().then(() => {
+    console.log('Connected to mondoDb')
+    app.listen(PORT, () => {
+        console.log(`App is listening at http://localhost:${PORT}`);
+    })
 });
